050895612beec2f1fd6e69a6b2c04c1f8d99e976
Emily Backes <lucca@accela.net>
2019-02-22 16:53:37 -0800

    Fix post-worldgen acacia sapling growth

--- src/Common/com/bioxx/tfc/Blocks/Flora/BlockSapling2.java
+++ src/Common/com/bioxx/tfc/Blocks/Flora/BlockSapling2.java
@@ -1,8 +1,17 @@
 package com.bioxx.tfc.Blocks.Flora;
 
-import net.minecraft.util.IIcon;
+import java.util.Random;
 
+import net.minecraft.block.Block;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+import com.bioxx.tfc.TileEntities.TESapling;
+import com.bioxx.tfc.WorldGen.Generators.Trees.WorldGenCustomShortTrees;
 import com.bioxx.tfc.api.Constant.Global;
+import com.bioxx.tfc.api.TFCBlocks;
 
 public class BlockSapling2 extends BlockSapling
 {
@@ -12,5 +21,27 @@ public class BlockSapling2 extends BlockSapling
 		this.woodNames = new String[Global.WOOD_ALL.length - 16];
 		System.arraycopy(Global.WOOD_ALL, 16, woodNames, 0, Global.WOOD_ALL.length - 16);
 		this.icons = new IIcon[woodNames.length];
+	}
+
+	@Override
+	public void growTree(World world, int i, int j, int k, Random rand, long timestamp)
+	{
+		int meta = world.getBlockMetadata(i, j, k);
+		world.setBlockToAir(i, j, k);
+		WorldGenerator worldGen =
+			// should be true for post-worldgen treegen but working around tfc conventions
+			new WorldGenCustomShortTrees(false, meta, TFCBlocks.logNatural2, TFCBlocks.leaves2);
+
+		if (worldGen != null && !worldGen.generate(world, rand, i, j, k))
+		{
+			world.setBlock(i, j, k, this, meta, 3); // Flag3 = blockupdate and send to client
+			if (world.getTileEntity(i, j, k) instanceof TESapling)
+			{
+				TESapling te = (TESapling) world.getTileEntity(i, j, k);
+				te.growTime = timestamp;
+				te.enoughSpace = false;
+				te.markDirty();
+			}
+		}
 	}
 }
--- src/Common/com/bioxx/tfc/WorldGen/Generators/Trees/WorldGenCustomShortTrees.java
+++ src/Common/com/bioxx/tfc/WorldGen/Generators/Trees/WorldGenCustomShortTrees.java
@@ -11,27 +11,38 @@ import com.bioxx.tfc.api.TFCBlocks;
 
 public class WorldGenCustomShortTrees extends WorldGenerator
 {
+	private Block woodBlock;
+	private Block leafBlock;
 	private final int treeId;
 
 	public WorldGenCustomShortTrees(boolean flag, int id)
 	{
 		super(flag);
+		woodBlock = TFCBlocks.logNatural;
+		leafBlock = TFCBlocks.leaves;
+		treeId=id;
+	}
+	public WorldGenCustomShortTrees(boolean flag, int id, Block logs, Block leaves)
+	{
+		super(flag);
+		woodBlock = logs;
+		leafBlock = leaves;
 		treeId=id;
 	}
 	@Override
 	public boolean generate(World world, Random random, int xCoord, int yCoord, int zCoord)
 	{
-		int l = random.nextInt(3) + 4;
-		if (yCoord < 1 || yCoord + l + 1 > world.getHeight())
+		int height = random.nextInt(3) + 4;
+		if (yCoord < 1 || yCoord + height + 1 > world.getHeight())
 			return false;
 
 		boolean flag = true;
-		for (int i1 = yCoord; i1 <= yCoord + 1 + l; i1++)
+		for (int i1 = yCoord; i1 <= yCoord + 1 + height; i1++)
 		{
 			byte byte0 = 1;
 			if (i1 == yCoord)
 				byte0 = 0;
-			if (i1 >= yCoord + 1 + l - 2)
+			if (i1 >= yCoord + 1 + height - 2)
 				byte0 = 2;
 
 			for (int i2 = xCoord - byte0; i2 <= xCoord + byte0 && flag; i2++)
@@ -55,28 +66,28 @@ public class WorldGenCustomShortTrees extends WorldGenerator
 		if (!flag)
 			return false;
 
-		if (!(TFC_Core.isSoil(world.getBlock(xCoord, yCoord - 1, zCoord)))|| yCoord >= world.getHeight() - l - 1)
+		if (!(TFC_Core.isSoil(world.getBlock(xCoord, yCoord - 1, zCoord)))|| yCoord >= world.getHeight() - height - 1)
 			return false;
 
-		for (int k1 = yCoord - 3 + l; k1 <= yCoord + l; k1++)
+		for (int gy = yCoord - 3 + height; gy <= yCoord + height; gy++)
 		{
-			int j2 = k1 - (yCoord + l);
-			int i3 = 1 - j2 / 2;
-			for (int k3 = xCoord - i3; k3 <= xCoord + i3; k3++)
+			int ry = gy - (yCoord + height); // -3 to 0
+			int radiusAtLevel = 1 - ry / 2;  // 3 to 1
+			for (int gx = xCoord - radiusAtLevel; gx <= xCoord + radiusAtLevel; gx++)
 			{
-				int l3 = k3 - xCoord;
-				for (int i4 = zCoord - i3; i4 <= zCoord + i3; i4++)
+				int rx = gx - xCoord;
+				for (int gz = zCoord - radiusAtLevel; gz <= zCoord + radiusAtLevel; gz++)
 				{
-					int j4 = i4 - zCoord;
-					if ((Math.abs(l3) != i3 || Math.abs(j4) != i3 || random.nextInt(2) != 0 && j2 != 0) && world.isAirBlock(k3, k1, i4))
-						setBlockAndNotifyAdequately(world, k3, k1, i4, TFCBlocks.leaves, treeId);
+					int rz = gz - zCoord;
+					if ((Math.abs(rx) != radiusAtLevel || Math.abs(rz) != radiusAtLevel || random.nextInt(2) != 0 && ry != 0) && world.isAirBlock(gx, gy, gz))
+						setBlockAndNotifyAdequately(world, gx, gy, gz, leafBlock, treeId);
 				}
 			}
 		}
 
-		for (int l1 = 0; l1 < l; l1++)
+		for (int l1 = 0; l1 < height; l1++)
 		{
-			setBlockAndNotifyAdequately(world, xCoord, yCoord + l1, zCoord, TFCBlocks.logNatural, treeId);
+			setBlockAndNotifyAdequately(world, xCoord, yCoord + l1, zCoord, woodBlock, treeId);
 		}
 
 		return true;
